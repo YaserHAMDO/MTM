e70ff7f4b6c578b414e59c8561451270bef71921
yaser <yaser.bilgisayar@gmail.com>
2024-02-16 00:30:30 +0300

    15

--- app/src/main/java/com/example/mtm/activity/InternetActivity.java
+++ app/src/main/java/com/example/mtm/activity/InternetActivity.java
@@ -1,10 +1,15 @@
 package com.example.mtm.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,6 +18,7 @@ import com.example.mtm.adapter.InternetAdapter;
 import com.example.mtm.adapter.InternetSubListAdapter;
 import com.example.mtm.model.InternetModel;
 import com.example.mtm.model.InternetSubListModel;
+import com.example.mtm.model.MainActivityModel;
 import com.example.mtm.network.ApiService;
 import com.example.mtm.network.RetrofitClient;
 import com.example.mtm.response.InternetResponse;
@@ -22,9 +28,14 @@ import com.example.mtm.util.DataHolder;
 import com.example.mtm.util.Logger;
 import com.example.mtm.util.MyUtils;
 import com.example.mtm.util.PreferenceManager;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,8 +45,11 @@ public class InternetActivity extends AppCompatActivity implements InternetSubLi
 
     private static final String TAG = "InternetActivity";
 
-    private ImageView backIconImageView;
+    private ImageView backIconImageView, filterImageView;
     private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+
+    private MaterialDatePicker materialDatePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +59,61 @@ public class InternetActivity extends AppCompatActivity implements InternetSubLi
         init();
         setItemClickListeners();
         setData();
+        confDate();
+    }
+
+    private void confDate() {
+        MaterialDatePicker.Builder<Pair<Long, Long>> materialDateBuilder = MaterialDatePicker.Builder.dateRangePicker();
+        materialDateBuilder.setTitleText("Tarih Seç");
+        materialDateBuilder.setTheme(R.style.MaterialDatePicker);
+        materialDatePicker = materialDateBuilder.build();
+
+//        materialDatePicker.addOnPositiveButtonClickListener(
+//                new MaterialPickerOnPositiveButtonClickListener() {
+//                    @SuppressLint("SetTextI18n")
+//                    @Override
+//                    public void onPositiveButtonClick(Object selection) {
+//
+//                        // if the user clicks on the positive
+//                        // button that is ok button update the
+//                        // selected date
+////                        mShowSelectedDateText.setText("Selected Date is : " + materialDatePicker.getHeaderText());
+//                        // in the above statement, getHeaderText
+//                        // will return selected date preview from the
+//                        // dialog
+//                    }
+//                });
+
+        materialDatePicker.addOnPositiveButtonClickListener(
+                selection -> {
+                    Pair<Long, Long> selectionPair = (Pair<Long, Long>) materialDatePicker.getSelection();
+                    if (selectionPair != null) {
+                        long startDate = selectionPair.first;
+                        long endDate = selectionPair.second;
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                        String formattedStartDate = sdf.format(new Date(startDate));
+                        String formattedEndDate = sdf.format(new Date(endDate));
+//                        Toast.makeText(this, "Start Date: " + formattedStartDate + "\nEnd Date: " + formattedEndDate, Toast.LENGTH_SHORT).show();
+                        recyclerView.setAdapter(null);
+                        progressBar.setVisibility(View.VISIBLE);
+                        internet(formattedStartDate, formattedEndDate);
+                    }
+                });
     }
 
     private void init() {
         backIconImageView = findViewById(R.id.backIconImageView);
+        filterImageView = findViewById(R.id.filterImageView);
         recyclerView = findViewById(R.id.recyclerView);
+        progressBar = findViewById(R.id.progressBar);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
     }
 
     private void setItemClickListeners() {
         backIconImageView.setOnClickListener(view -> getOnBackPressedDispatcher().onBackPressed());
+        filterImageView.setOnClickListener(view -> materialDatePicker.show(getSupportFragmentManager(), "MATERIAL_DATE_PICKER"));
+
     }
 
     private void setData() {
@@ -83,7 +143,7 @@ public class InternetActivity extends AppCompatActivity implements InternetSubLi
             ));
 
         }
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+
         recyclerView.setAdapter(new InternetAdapter(this, items, this));
 
     }
@@ -154,6 +214,74 @@ public class InternetActivity extends AppCompatActivity implements InternetSubLi
 
     }
 
+    private void internet(String startDate, String endDate) {
+
+        PreferenceManager preferenceManager = new PreferenceManager(getApplicationContext());
+
+        ApiService apiService = RetrofitClient.getClient(2).create(ApiService.class);
+
+        Call<InternetResponse> call = apiService.internet(
+                "Bearer " + preferenceManager.getString(Constants.KEY_ACCESS_TOKEN),
+                0,
+                10000,
+                22632,
+                false,
+                false,
+                true,
+                true,
+                true,
+//                MyUtils.getPreviousDate(1),
+//                MyUtils.getCurrentDate(),
+                startDate,
+                endDate,
+                "07:00:00",
+                "23:59:00",
+                "UNIGNORED",
+                true,
+                true,
+                true
+        );
+
+        call.enqueue(new Callback<InternetResponse>() {
+            @Override
+            public void onResponse(Call<InternetResponse> call, Response<InternetResponse> response) {
+
+                Logger.getInstance().logDebug(TAG, "visualMedia", 2, response.body());
+
+                progressBar.setVisibility(View.GONE);
+
+                if (response.isSuccessful()) {
+
+                    InternetResponse result = response.body();
+
+                    DataHolder.getInstance().setInternetModel(result);
+
+                    setData();
+
+
+
+
+                } else {
+
+                    if (response.code() == 403) {
+//                        forbiddenPopup();
+                    }
+
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<InternetResponse> call, Throwable t) {
+
+                progressBar.setVisibility(View.GONE);
+
+                Logger.getInstance().logDebug(TAG, "visualMedia", 3, t.getMessage());
+            }
+        });
+
+
+    }
 
     @Override
     public void onItemClickInternetSubList(String menuId, String subMenuId) {
