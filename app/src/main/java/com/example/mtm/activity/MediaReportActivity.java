a77f601655a38c22aa63ff5b51f39f5b639f6b4b
Android <yaser.bilgisayar@gmail.com>
2024-02-15 16:28:43 +0300

    13

---
+++ app/src/main/java/com/example/mtm/activity/MediaReportActivity.java
@@ -0,0 +1,81 @@
+package com.example.mtm.activity;
+
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
+
+import com.example.mtm.R;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+public class MediaReportActivity extends AppCompatActivity {
+
+    private Button mPickDateButton;
+    private TextView mShowSelectedDateText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_report);
+
+        // now register the text view and the button with
+        // their appropriate IDs
+        mPickDateButton = findViewById(R.id.pick_date_button);
+        mShowSelectedDateText = findViewById(R.id.show_selected_date);
+
+        // now create instance of the material date picker
+        // builder make sure to add the "dateRangePicker"
+        // which is material date range picker which is the
+        // second type of the date picker in material design
+        // date picker we need to pass the pair of Long
+        // Long, because the start date and end date is
+        // store as "Long" type value
+        MaterialDatePicker.Builder<Pair<Long, Long>> materialDateBuilder = MaterialDatePicker.Builder.dateRangePicker();
+
+        // now define the properties of the
+        // materialDateBuilder
+        materialDateBuilder.setTitleText("SELECT A DATE");
+
+        // now create the instance of the material date
+        // picker
+        MaterialDatePicker materialDatePicker = materialDateBuilder.build();
+
+        // handle select date button which opens the
+        // material design date picker
+        mPickDateButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // getSupportFragmentManager() to
+                        // interact with the fragments
+                        // associated with the material design
+                        // date picker tag is to get any error
+                        // in logcat
+                        materialDatePicker.show(getSupportFragmentManager(), "MATERIAL_DATE_PICKER");
+                    }
+                });
+
+        // now handle the positive button click from the
+        // material design date picker
+        materialDatePicker.addOnPositiveButtonClickListener(
+                new MaterialPickerOnPositiveButtonClickListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onPositiveButtonClick(Object selection) {
+
+                        // if the user clicks on the positive
+                        // button that is ok button update the
+                        // selected date
+                        mShowSelectedDateText.setText("Selected Date is : " + materialDatePicker.getHeaderText());
+                        // in the above statement, getHeaderText
+                        // will return selected date preview from the
+                        // dialog
+                    }
+                });
+    }
+}
