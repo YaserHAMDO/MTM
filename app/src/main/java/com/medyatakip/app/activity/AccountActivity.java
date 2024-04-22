ef5d652cf2e2d66b8bd5941c794828cffdcc75ce
yaser_acer <yaser.bilgisaysar@gmail.com>
2024-04-22 16:41:15 +0300

    test

--- app/src/main/java/com/medyatakip/app/activity/AccountActivity.java
+++ app/src/main/java/com/medyatakip/app/activity/AccountActivity.java
@@ -26,6 +26,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+// test
 public class AccountActivity extends AppCompatActivity {
 
     private static final String TAG = "AccountActivity";
